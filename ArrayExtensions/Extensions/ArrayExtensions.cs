b7f8dc441a125b66926814a95ef88d4a842408fc
Tim Maes <91606949+Tim-Maes@users.noreply.github.com>
2023-10-03 15:15:46 +0200

    Add project files.

---
+++ ArrayExtensions/Extensions/ArrayExtensions.cs
@@ -0,0 +1,98 @@
+﻿namespace ArrayExtensions;
+
+public static class ArrayExtensions
+{
+    /// <summary>
+    /// Appends a single item at the end of an array.
+    /// </summary>
+    /// <typeparam name="T">The type of the elements contained in the array.</typeparam>
+    /// <param name="arr">The array to append to.</param>
+    /// <param name="item">The item to append.</param>
+    /// <returns>A new array.</returns>
+    public static T[] Add<T>(this T[] arr, T item) => arr.Concat(new[] { item }).ToArray();
+
+    /// <summary>
+    /// Checks if all items in the array are equal.
+    /// </summary>
+    /// <typeparam name="T">The type of the elements contained in the array.</typeparam>
+    /// <param name="arr">The array to check.</param>
+    /// <returns>True if all items are equal, otherwise false.</returns>
+    public static bool AllEqual<T>(this T[] arr) => arr.Distinct().Count() == 1;
+
+    /// <summary>
+    /// Checks if there is a null value present in the array.
+    /// </summary>
+    /// <typeparam name="T">The type of elements contained in the array</typeparam>
+    /// <param name="arr"></param>
+    /// <returns></returns>
+    public static bool AnyNull<T>(this T[] arr) where T : class => arr.Any(item => item == null);
+
+    /// <summary>
+    /// Appends multiple items at the end of an array.
+    /// </summary>
+    public static T[] AddRange<T>(this T[] arr, params T[] items) => arr.Concat(items).ToArray();
+
+    /// <summary>
+    /// Removes an item at a specified index.
+    /// </summary>
+    public static T[] RemoveAt<T>(this T[] arr, int index)
+    {
+        if (index < 0 || index >= arr.Length) throw new IndexOutOfRangeException();
+        return arr.Where((_, i) => i != index).ToArray();
+    }
+
+    /// <summary>
+    /// Inserts an item at a specified index.
+    /// </summary>
+    public static T[] InsertAt<T>(this T[] arr, int index, T item)
+    {
+        if (index < 0 || index > arr.Length) throw new IndexOutOfRangeException();
+        return arr.Take(index).Concat(new[] { item }).Concat(arr.Skip(index)).ToArray();
+    }
+
+    /// <summary>
+    /// Shuffles the array elements randomly.
+    /// </summary>
+    public static T[] Shuffle<T>(this T[] arr)
+    {
+        var rng = new Random();
+        return arr.OrderBy(_ => rng.Next()).ToArray();
+    }
+
+    /// <summary>
+    /// Checks if the array is empty.
+    /// </summary>
+    public static bool IsEmpty<T>(this T[] arr) => arr.Length == 0;
+
+    /// <summary>
+    /// Checks if the array is null or empty.
+    /// </summary>
+    public static bool IsNullOrEmpty<T>(this T[] arr) => arr == null || arr.Length == 0;
+
+    /// <summary>
+    /// Gets the element at the given index if it exists, otherwise return a default value.
+    /// </summary>
+    public static T SafeGet<T>(this T[] arr, int index, T defaultValue = default)
+    {
+        return (index >= 0 && index < arr.Length) ? arr[index] : defaultValue;
+    }
+
+    /// <summary>
+    /// Finds all indices of items that match a given predicate.
+    /// </summary>
+    public static int[] FindIndices<T>(this T[] arr, Predicate<T> match)
+        => arr.Select((item, index) => match(item) ? index : -1)
+                  .Where(index => index != -1)
+                  .ToArray();
+
+    /// <summary>
+    /// Executes an action for each element in the array.
+    /// </summary>
+    public static void ForEach<T>(this T[] arr, Action<T> action)
+    {
+        foreach (var item in arr)
+        {
+            action(item);
+        }
+    }
+}
---
+++ ArrayExtensions/Extensions/StringArrayExtensions.cs
@@ -0,0 +1,54 @@
+﻿namespace ArrayExtensions;
+
+public static class StringArrayExtensions
+{
+    /// <summary>
+    /// Checks if any item is null or empty in the array.
+    /// </summary>
+    /// <param name="arr">The array to check.</param>
+    /// <returns>True if atleast one item is null or empty.</returns>
+    public static bool AnyNullOrEmpty(this string[] arr) => arr.Any(string.IsNullOrEmpty);
+
+    /// <summary>
+    /// Checks if any item is null or whitespace in the array.
+    /// </summary>
+    /// <param name="arr">The array to check.</param>
+    /// <returns>True if atleast one item is null or empty.</returns>
+    public static bool AnyNullOrWhiteSpace(this string[] arr) => arr.Any(string.IsNullOrWhiteSpace);
+
+    /// <summary>
+    /// Trims all items in the array
+    /// </summary>
+    /// <param name="arr">The array to trim all items.</param>
+    /// <returns>Array with trimmed items.</returns>
+    public static string[] TrimAll(this string[] arr) => arr.Select(s => s.Trim()).ToArray();
+
+    /// <summary>
+    /// Removes all empty and null items from the array.
+    /// </summary>
+    /// <param name="arr">The array to remove null or empty items from.</param>
+    /// <returns>Array without null or empty values.</returns>
+    public static string[] RemoveNullOrEmpty(this string[] arr) => arr.Where(s => !string.IsNullOrEmpty(s)).ToArray();
+
+    /// <summary>
+    /// Removes all whitespace and null items from the array.
+    /// </summary>
+    /// <param name="arr">The array to remove whitespace or empty items from.</param>
+    /// <returns>Array without null or empty values.</returns>
+    public static string[] RemoveNullOrWhiteSpace(this string[] arr) => arr.Where(s => !string.IsNullOrWhiteSpace(s)).ToArray();
+
+    /// <summary>
+    /// Checks if the array has duplicate values.
+    /// </summary>
+    /// <param name="arr">The array to check.</param>
+    /// <returns>True if any duplicate values present.</returns>
+    public static bool HasDuplicates(this string[] arr) => arr.GroupBy(s => s).Any(g => g.Count() > 1);
+
+    /// <summary>
+    /// Concatenate all strings with a given separator.
+    /// </summary>
+    /// <param name="arr">The array to concatenate.</param>
+    /// <param name="separator">The separator.</param>
+    /// <returns>Concatenated string from all array elements.</returns>
+    public static string ConcatenateWithSeparator(this string[] arr, string separator) => string.Join(separator, arr);
+}
